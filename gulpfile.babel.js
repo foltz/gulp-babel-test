445ed419f4524e602ca6b561c3302bc9f6486f33
foltz <foltz@lxis.net>
2015-10-09 23:42:48 -0500

    updates to gulp file
     - reorganized code
     - setup extensions for jsx modules
     - setup minify & uglify tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,188 +1,175 @@
 import gulp from 'gulp';
 import runSequence from 'run-sequence';
 
-//import merge from 'merge-stream';
-
 import gulpFilter from 'gulp-filter';
 
 import babel from 'gulp-babel';
 import sourcemaps from 'gulp-sourcemaps';
 
+
+import concat from 'gulp-concat';
+import rename from 'gulp-rename';
 import del from 'del';
 
 import nodemon from 'gulp-nodemon';
 
+import browserify from 'browserify';
+import babelify from 'babelify';
 
-gulp.task('clean-dist', () => del(['dist/**/*']));
+import uglifyify from 'uglifyify';
+import minifyify from 'minifyify';
 
-gulp.task('clean-css', () => del(['dist/css']));
+import source from 'vinyl-source-stream';
 
-gulp.task('copy-css', () => {
+let tasks = {
 
-	return gulp.src('static/css/*')
-			.pipe(gulp.dest('dist/css'));
-});
 
-gulp.task('build-css', (done) => {
-	runSequence('clean-css', 'copy-css', () => { done(); });
-});
+	cleanDist : () => del(['dist/**/*']),
+	cleanCss : () => del(['dist/css']),
+	cleanBuild : () => del(['build']),
+
+	copyCss: () => {
+
+		return gulp.src('static/css/*')
+				.pipe(gulp.dest('dist/css'));
+	},
+
+	buildCss: (done) => {
+		runSequence('clean-css', 'copy-css', () => { done(); });
+	},
+
+	buildDist: (done) => {
+
+		runSequence('clean-dist', 'build-static', () => {
+
+			var jsFilter = gulpFilter('**/*.{js,jsx}', {restore:true});
+
+			gulp.src('src/**/*')
+
+					.pipe(jsFilter)
+					.pipe(sourcemaps.init())
+					.pipe(babel())
+					.pipe(sourcemaps.write("."))
+					.pipe(jsFilter.restore)
+
+					.pipe(gulp.dest('dist'));
+
+			done();
+		});
+	},
+
+	buildClientRoutes: () => {
+
+		var bundler = browserify({
+			entries: ['./src/routes/client-router.js'], // Only need initial file, browserify finds the deps
+			transform: [babelify], // We want to convert JSX to normal javascript
+			extensions: ['.js', '.jsx'],
+			debug: true, // Gives us sourcemapping
+			//cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+
+		});
+		//bundler.plugin('minifyify', {map: 'bundle.map.json'});
+
+		bundler.bundle() // Create the initial bundle when starting the task
+				.pipe(source('client-router.js'))
+				.pipe(gulp.dest('build'))
+	},
+
+	uglifyClientRoutes: () => {
+
+		var bundler = browserify({
+			entries: ['./src/routes/client-router.js'], // Only need initial file, browserify finds the deps
+			transform: [babelify], // We want to convert JSX to normal javascript
+			extensions: ['.js', '.jsx'],
+			//debug: true, // Gives us sourcemapping
+			//cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+
+		});
+
+		bundler.transform({
+			global: true
+		}, 'uglifyify')
+
+		bundler.bundle() // Create the initial bundle when starting the task
+				.pipe(source('client-router.js'))
+				.pipe(rename('client-router.ugl.js'))
+				.pipe(gulp.dest('build'))
+	},
+
+	minifyClientRoutes: () => {
+
+		var outputFile = 'build/client-router.min.js';
+
+		var bundler = browserify({
+			entries: ['./src/routes/client-router.js'], // Only need initial file, browserify finds the deps
+			transform: [babelify], // We want to convert JSX to normal javascript
+			extensions: ['.js', '.jsx'],
+			debug: true, // Gives us sourcemapping
+			//cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+
+		});
+
+		//bundler.add('./src/routes/client-router.js');
+
+		bundler.plugin('minifyify',
+				{
+					map: 'bundle.map.json',
+					output: outputFile
+				});
+
+		bundler.bundle()
+				.pipe(source('client-router.js'))
+				.pipe(rename('client-router.min.js'))
+				.pipe(gulp.dest('build'))
+				//.pipe(fs.createWriteStream(outputFile));
+	},
+
+	runDevServer: () => {
+
+		runSequence('build-dist', () => {
+
+			setTimeout(() => {
+
+				nodemon({ script: 'dist/app.js'
+					//, ext: 'html js'
+					//, ignore: ['ignored.js']
+					//, tasks: ['lint']
+				}).on('restart', function () {
+					console.log('restarted!');
+					//runSequence('watch');
+				});
+
+			}, 500);
+
+
+
+		});
+	},
+
+	runWatch: () => {
+
+		gulp.watch('src/**/*', ['build-dist']);
+		gulp.watch('static/**/*', ['build-static']);
+	}
+
+};
+
+gulp.task('clean-dist', tasks.cleanDist);
+gulp.task('clean-css', tasks.cleanCss);
+gulp.task('clean-build', tasks.cleanBuild);
+
+gulp.task('copy-css', tasks.copyCss);
+
+gulp.task('build-css', tasks.buildCss);
 
 // - this will make more sense once there are other types of static content....
 gulp.task('build-static', ['build-css']);
+gulp.task('build-dist', tasks.buildDist);
+gulp.task('build-client', tasks.buildClientRoutes);
+gulp.task('ugl-client', tasks.uglifyClientRoutes);
+gulp.task('min-client', tasks.minifyClientRoutes);
 
+gulp.task('run-dev', ['run-watch'], tasks.runDevServer);
+gulp.task('run-watch', tasks.runWatch);
 
-gulp.task('build-dist', (done) => {
-
-	runSequence('clean-dist', 'build-static', () => {
-
-		var jsFilter = gulpFilter('**/*.{js,jsx}', {restore:true});
-
-		gulp.src('src/**/*')
-
-				.pipe(jsFilter)
-				.pipe(sourcemaps.init())
-				.pipe(babel())
-				.pipe(sourcemaps.write("."))
-				.pipe(jsFilter.restore)
-
-				.pipe(gulp.dest('dist'));
-
-		done();
-	});
-});
-
-
-//gulp.task('build', ['clean', 'dist-js', 'dist-assets']);
-
-gulp.task('watch', () => {
-	gulp.watch('src/**/*', ['build-dist']);
-	gulp.watch('static/**/*', ['build-static']);
-});
-
-gulp.task('dev', ['watch'], () => {
-
-	runSequence('build-dist', () => {
-
-		setTimeout(() => {
-
-			nodemon({ script: 'dist/app.js'
-				//, ext: 'html js'
-				//, ignore: ['ignored.js']
-				//, tasks: ['lint']
-			}).on('restart', function () {
-				console.log('restarted!');
-				//runSequence('watch');
-			});
-
-		}, 500);
-
-
-
-	});
-});
-
-
-gulp.task('default', ['watch']);
-
-
-import browserify from 'browserify';
-import watchify from 'watchify';
-import babelify from 'babelify';
-
-import source from 'vinyl-source-stream';
-import buffer from 'vinyl-buffer';
-import merge from 'utils-merge';
-import gutil from 'gulp-util';
-
-import rename from 'gulp-rename';
-import uglify from 'gulp-uglify'
-
-import chalk from 'chalk';
-
-gulp.task('browser', function () {
-	// set up the browserify instance on a task basis
-	//var b = browserify({
-	//	//entries: 'js/client-router.js',
-	//	debug: true
-	//});
-	//
-	//return b.bundle()
-	//		.pipe(source('src/js/client-router.js'))
-	//		.pipe(buffer())
-	//		.pipe(sourcemaps.init({loadMaps: true}))
-	//		// Add transformation tasks to the pipeline here.
-	//		//.pipe(uglify())
-	//		.on('error', gutil.log)
-	//		.pipe(sourcemaps.write('./'))
-	//		.pipe(gulp.dest('./dist/js'));
-
-	var args = merge(watchify.args, { debug: true });
-	var bundler = watchify(browserify('dist/routes/client-router.js', args))
-			.transform(babelify, { /* opts */ });
-
-	bundle_js(bundler)
-
-	bundler.on('update', function () {
-		bundle_js(bundler)
-	})
-
-});
-
-function bundle_js(bundler) {
-	return bundler.bundle()
-			.on('error', map_error)
-			.pipe(source('dist/routes/client-router.js'))
-			.pipe(buffer())
-			.pipe(gulp.dest('app/dist'))
-			.pipe(rename('app.min.js'))
-			.pipe(sourcemaps.init({ loadMaps: true }))
-			// capture sourcemaps from transforms
-			.pipe(uglify())
-			.pipe(sourcemaps.write('.'))
-			.pipe(gulp.dest('app/dist'))
-}
-
-function map_error(err) {
-	if (err.fileName) {
-		// regular error
-		gutil.log(chalk.red(err.name)
-				+ ': '
-				+ chalk.yellow(err.fileName.replace(__dirname + '/src/js/', ''))
-				+ ': '
-				+ 'Line '
-				+ chalk.magenta(err.lineNumber)
-				+ ' & '
-				+ 'Column '
-				+ chalk.magenta(err.columnNumber || err.column)
-				+ ': '
-				+ chalk.blue(err.description))
-	} else {
-		// browserify error..
-		gutil.log(chalk.red(err.name)
-				+ ': '
-				+ chalk.yellow(err.message))
-	}
-
-	this.end()
-}
-
-
-
-//var gulp = require('gulp');
-//var browserify = require('browserify');
-//var babelify = require('babelify');
-//var source = require('vinyl-source-stream');
-//
-//gulp.task('build', function () {
-//	return browserify({entries: 'src/components/App/App.js', extensions: ['.js'], debug: true})
-//			.transform(babelify)
-//			.bundle()
-//			.pipe(source('bundle.js'))
-//			.pipe(gulp.dest('dist'));
-//});
-//
-//gulp.task('watch', ['build'], function () {
-//	gulp.watch('*.jsx', ['build']);
-//});
-//
-//gulp.task('default', ['watch']);
+gulp.task('default', ['run-watch']);
